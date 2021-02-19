9ed72ee2554f508fd1fe00e1a1206625e1e03698
Tom Ball <tball4327@gmail.com>
2021-02-19 14:54:59 -0600

    analysis section in progress...

--- EmployeeSQL/employee_analytics.sql
+++ EmployeeSQL/employee_analytics.sql
@@ -91,19 +91,36 @@ REFERENCES "employees" ("emp_no");
 -- Begin Data Analysis section
 
 -- List the following details of each employee: employee number, last name, first name, sex, and salary.
-select "employees".emp_no, "employees".last_name, "employees".first.name, "employees".sex, "salaries".salary
+select "employees".emp_no, "employees".last_name, "employees".first_name, "employees".sex, "salaries".salary
 from "employees"
 left join "salaries"
-on emp_no
+on "employees".emp_no = "salaries".emp_no
 
 -- List first name, last name, and hire date for employees who were hired in 1986.
-
+select first_name, last_name, hire_date from employees
+where hire_date like '%1986'
 
 -- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
 
+-----COME BACK TO THIS
+
+select "dept_manager".dept_no, "departments".dept_name, "dept_manager".emp_no, "employees".last_name, "employees".first_name
+from "employees"
+left join "dept_manager"
+on "employees".emp_no = "dept_manager".emp_no
+left join "departments"
+on "dept_manager".dept_no = "departments".dept_name
+
+
+
 
 -- List the department of each employee with the following information: employee number, last name, first name, and department name.
-
+select "employees".emp_no, "employees".last_name, "employees".first_name, "departments".dept_name
+from "employees"
+left join "dept_emp"
+on "employees".emp_no = "dept_emp".emp_no
+left join "departments"
+on "dept_emp".dept_no = "departments".dept_no
 
 -- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 select * from "employees"
@@ -111,8 +128,13 @@ where first_name = 'Hercules' and last_name like 'B%'
 
 -- List all employees in the Sales department, including their employee number, last name, first name, and department name.
 -- sales dept is coded as d007
-select * from "employees"
-where 
+select "employees".emp_no, "employees".last_name, "employees".first_name, "departments".dept_name
+from "employees"
+left join "dept_emp"
+on "employees".emp_no = "dept_emp".emp_no
+left join "departments" 
+on "dept_emp".dept_no = "departments".dept_no
+where dept_name = 'Sales'
 
 -- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
